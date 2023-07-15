b83bca9f99bf757e22f39ae07dee1e2847ff3969
joshua-pathkraft <126854752+joshua-pathkraft@users.noreply.github.com>
2023-07-15 23:55:20 +0530

    Resolved Some Minor Bugs

--- controller/history.js
+++ controller/history.js
@@ -1,5 +1,6 @@
 const { Types } = require('mongoose');
 const History = require('../models/history');
+const User = require('../models/users');
 
 exports.createHistory = async (req, res, next) => {
   try {
@@ -24,7 +25,7 @@ exports.createHistory = async (req, res, next) => {
 
 exports.getHistoryById = async (req, res, next) => {
   try {
-    userId = req.body.userId;
+    const userId = req.params.id;
 
     const history = await History.aggregate([
       { $match: { userId: new Types.ObjectId(userId) } },
@@ -37,8 +38,20 @@ exports.getHistoryById = async (req, res, next) => {
         },
       },
       {
+        $lookup: {
+          from: 'users',
+          localField: 'userId',
+          foreignField: '_id',
+          as: 'user',
+        },
+      },
+      {
+        $unwind: '$user',
+      },
+      {
         $project: {
           userId: '$userId',
+          userName: '$user.name',
           date: '$date',
           name: '$product_details.name',
           price: '$product_details.price',
@@ -54,8 +67,6 @@ exports.getHistoryById = async (req, res, next) => {
       throw error;
     }
 
-    console.log(JSON.stringify(history, 0, 2));
-
     return res
       .status(200)
       .json({ message: 'History Found Successfully', history: history });
@@ -66,7 +77,7 @@ exports.getHistoryById = async (req, res, next) => {
 
 exports.totalAmountByUser = async (req, res, next) => {
   try {
-    const userId = req.body.userId;
+    const userId = req.params.id;
 
     const TotalAmount = await History.aggregate([
       { $match: { userId: new Types.ObjectId(userId) } },
@@ -86,8 +97,20 @@ exports.totalAmountByUser = async (req, res, next) => {
         },
       },
       {
+        $lookup: {
+          from: 'users',
+          localField: 'userId',
+          foreignField: '_id',
+          as: 'user',
+        },
+      },
+      {
+        $unwind: '$user',
+      },
+      {
         $project: {
           userId: '$userId',
+          userName: '$user.name',
           date: '$date',
           name: '$product_details.name',
           price: '$product_details.price',
@@ -124,6 +147,8 @@ exports.totalAmountByUser = async (req, res, next) => {
       // },
     ]);
 
+    // console.log(TotalAmount);
+
     const totalAmount = await TotalAmount.map((item) => {
       const totalPrice = item.price.map(
         (price, index) => price * item.qty[index]
@@ -139,10 +164,113 @@ exports.totalAmountByUser = async (req, res, next) => {
       return accumulator + innerSum;
     }, 0);
 
-    return res
-      .status(200)
-      .json({ message: 'Total Amount Fetched Successfully', totalAmount: sum });
+    return res.status(200).json({
+      message: 'Total Amount Fetched Successfully',
+      totalAmount: sum,
+      userName: TotalAmount[0].userName,
+    });
   } catch (err) {
     console.log(err);
   }
 };
+
+// exports.totalAmountForAllUser = async (req, res, next) => {
+//   try {
+//     let userIds = [];
+//     const users = await User.find();
+//     console.log(users);
+
+//     await users.forEach((obj) => {
+//       userIds.push(obj._id);
+//     });
+
+//     console.log(userIds);
+
+//     const userId = req.body.userId;
+
+//     const TotalAmount = await History.aggregate([
+//       {
+//         $match: {
+//           userId: userIds.map((id) => {
+//             return new Types.ObjectId(id);
+//           }),
+//         },
+//       },
+//       // {
+//       //   $group: {
+//       //     _id: '$createdAt',
+//       //     data: { $push: '$$ROOT' },
+//       //   },
+//       // },
+//       // { $unwind: '$data' },
+//       {
+//         $lookup: {
+//           from: 'products',
+//           localField: 'items.item',
+//           foreignField: '_id',
+//           as: 'product_details',
+//         },
+//       },
+//       {
+//         $project: {
+//           userId: '$userId',
+//           date: '$date',
+//           name: '$product_details.name',
+//           price: '$product_details.price',
+//           qty: '$items.qty',
+//         },
+//       },
+//       { $sort: { date: -1 } },
+//       // { $unwind: '$price' },
+//       // { $unwind: '$name' },
+//       // { $unwind: '$qty' },
+//       // {
+//       //   $project: {
+//       //     userId: 1,
+//       //     createdAt: 1,
+//       //     name: '$name',
+//       //     price: '$price',
+//       //     qty: '$qty',
+//       //     subTotal: { $multiply: ['$price', '$qty'] },
+//       //   },
+//       // },
+//       // {
+//       //   $group: {
+//       //     _id: '$createdAt',
+//       //     items: { $push: '$$ROOT' },
+//       //     TotalAmount: { $sum: '$subTotal' },
+//       //   },
+//       // },
+//       // {
+//       //   $project: {
+//       //     userId: 1,
+//       //     items: '$items',
+//       //     Total: '$TotalAmount',
+//       //   },
+//       // },
+//     ]);
+
+//     console.log(TotalAmount);
+
+//     const totalAmount = await TotalAmount.map((item) => {
+//       const totalPrice = item.price.map(
+//         (price, index) => price * item.qty[index]
+//       );
+//       return totalPrice;
+//     });
+
+//     const sum = totalAmount.reduce((accumulator, innerArray) => {
+//       const innerSum = innerArray.reduce(
+//         (innerAccumulator, currentValue) => innerAccumulator + currentValue,
+//         0
+//       );
+//       return accumulator + innerSum;
+//     }, 0);
+
+//     return res
+//       .status(200)
+//       .json({ message: 'Total Amount Fetched Successfully', totalAmount: sum });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
--- router/history.js
+++ router/history.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const historyController = require('../controller/history');
 
-router.get('', historyController.getHistoryById);
+router.get('/:id', historyController.getHistoryById);
+
+router.get('/total/:id', historyController.totalAmountByUser);
+
+// router.get('', historyController.totalAmountForAllUser);
 
 router.post('', historyController.createHistory);
-
-router.get('/total', historyController.totalAmountByUser);
 
 module.exports = router;
