3293fa6bff472c79fdeb50fb0d1434a07035c8ff
joshua-pathkraft <126854752+joshua-pathkraft@users.noreply.github.com>
2023-07-16 01:07:18 +0530

    Added CORS Permission

--- app.js
+++ app.js
@@ -10,6 +10,20 @@ const adminRouter = require('./router/admin');
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS,GET,POST,PUT,PATCH,DELETE'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type',
+    'Authorization'
+  );
+  next();
+});
+
 dotenv.config({ path: './config/.env' });
 
 app.use('/users', userRouter);
