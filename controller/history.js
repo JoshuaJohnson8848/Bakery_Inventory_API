79af033e51d6a4cb3b9672b6889a611d3868c2e8
joshua-pathkraft <126854752+joshua-pathkraft@users.noreply.github.com>
2023-07-29 20:58:01 +0530

    Added Get Total Amounts of All Users

--- controller/history.js
+++ controller/history.js
@@ -1,4 +1,4 @@
-const { Types } = require('mongoose');
+const { Types, default: mongoose } = require('mongoose');
 const History = require('../models/history');
 const User = require('../models/users');
 
@@ -81,13 +81,6 @@ exports.totalAmountByUser = async (req, res, next) => {
 
     const TotalAmount = await History.aggregate([
       { $match: { userId: new Types.ObjectId(userId) } },
-      // {
-      //   $group: {
-      //     _id: '$createdAt',
-      //     data: { $push: '$$ROOT' },
-      //   },
-      // },
-      // { $unwind: '$data' },
       {
         $lookup: {
           from: 'products',
@@ -118,36 +111,7 @@ exports.totalAmountByUser = async (req, res, next) => {
         },
       },
       { $sort: { date: -1 } },
-      // { $unwind: '$price' },
-      // { $unwind: '$name' },
-      // { $unwind: '$qty' },
-      // {
-      //   $project: {
-      //     userId: 1,
-      //     createdAt: 1,
-      //     name: '$name',
-      //     price: '$price',
-      //     qty: '$qty',
-      //     subTotal: { $multiply: ['$price', '$qty'] },
-      //   },
-      // },
-      // {
-      //   $group: {
-      //     _id: '$createdAt',
-      //     items: { $push: '$$ROOT' },
-      //     TotalAmount: { $sum: '$subTotal' },
-      //   },
-      // },
-      // {
-      //   $project: {
-      //     userId: 1,
-      //     items: '$items',
-      //     Total: '$TotalAmount',
-      //   },
-      // },
     ]);
-
-    // console.log(TotalAmount);
 
     const totalAmount = await TotalAmount.map((item) => {
       const totalPrice = item.price.map(
@@ -174,103 +138,123 @@ exports.totalAmountByUser = async (req, res, next) => {
   }
 };
 
-// exports.totalAmountForAllUser = async (req, res, next) => {
-//   try {
-//     let userIds = [];
-//     const users = await User.find();
-//     console.log(users);
+exports.totalAmountForAllUser = async (req, res, next) => {
+  try {
+    let userIds = [];
+    const users = await User.find();
 
-//     await users.forEach((obj) => {
-//       userIds.push(obj._id);
-//     });
+    users.forEach((obj) => {
+      userIds.push(obj._id);
+    });
 
-//     console.log(userIds);
+    const TotalAmount = await History.aggregate([
+      {
+        $match: {
+          userId: {
+            $in: userIds.map((id) => new Types.ObjectId(id)),
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: 'products',
+          localField: 'items.item',
+          foreignField: '_id',
+          as: 'product_details',
+        },
+      },
+      {
+        $project: {
+          userId: '$userId',
+          date: '$date',
+          name: '$product_details.name',
+          price: '$product_details.price',
+          qty: '$items.qty',
+        },
+      },
+      {
+        $addFields: {
+          multipliedPrice: {
+            $map: {
+              input: '$price',
+              as: 'singlePrice',
+              in: {
+                $multiply: [
+                  '$$singlePrice',
+                  {
+                    $arrayElemAt: [
+                      '$qty',
+                      { $indexOfArray: ['$price', '$$singlePrice'] },
+                    ],
+                  },
+                ],
+              },
+            },
+          },
+        },
+      },
+      {
+        $addFields: {
+          totalMultipliedPrice: {
+            $reduce: {
+              input: '$multipliedPrice',
+              initialValue: 0,
+              in: { $add: ['$$value', '$$this'] },
+            },
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'userId',
+          foreignField: '_id',
+          as: 'User_Name',
+        },
+      },
+      {
+        $project: {
+          userName: '$User_Name.name',
+          userId: '$userId',
+          date: '$date',
+          name: '$product_details.name',
+          price: '$product_details.price',
+          qty: '$items.qty',
+          amount: '$totalMultipliedPrice',
+        },
+      },
+      {
+        $unwind: '$userName',
+      },
+      {
+        $group: {
+          _id: '$userId',
+          data: {
+            $push: '$$ROOT',
+          },
+          sum: { $sum: '$amount' },
+        },
+      },
+      {
+        $project: {
+          userName: { $arrayElemAt: ['$data', 0] },
+          sum: 1,
+        },
+      },
+      {
+        $project: {
+          userId: '$userName.userId',
+          name: '$userName.userName',
+          sum: 1,
+        },
+      },
+    ]);
 
-//     const userId = req.body.userId;
-
-//     const TotalAmount = await History.aggregate([
-//       {
-//         $match: {
-//           userId: userIds.map((id) => {
-//             return new Types.ObjectId(id);
-//           }),
-//         },
-//       },
-//       // {
-//       //   $group: {
-//       //     _id: '$createdAt',
-//       //     data: { $push: '$$ROOT' },
-//       //   },
-//       // },
-//       // { $unwind: '$data' },
-//       {
-//         $lookup: {
-//           from: 'products',
-//           localField: 'items.item',
-//           foreignField: '_id',
-//           as: 'product_details',
-//         },
-//       },
-//       {
-//         $project: {
-//           userId: '$userId',
-//           date: '$date',
-//           name: '$product_details.name',
-//           price: '$product_details.price',
-//           qty: '$items.qty',
-//         },
-//       },
-//       { $sort: { date: -1 } },
-//       // { $unwind: '$price' },
-//       // { $unwind: '$name' },
-//       // { $unwind: '$qty' },
-//       // {
-//       //   $project: {
-//       //     userId: 1,
-//       //     createdAt: 1,
-//       //     name: '$name',
-//       //     price: '$price',
-//       //     qty: '$qty',
-//       //     subTotal: { $multiply: ['$price', '$qty'] },
-//       //   },
-//       // },
-//       // {
-//       //   $group: {
-//       //     _id: '$createdAt',
-//       //     items: { $push: '$$ROOT' },
-//       //     TotalAmount: { $sum: '$subTotal' },
-//       //   },
-//       // },
-//       // {
-//       //   $project: {
-//       //     userId: 1,
-//       //     items: '$items',
-//       //     Total: '$TotalAmount',
-//       //   },
-//       // },
-//     ]);
-
-//     console.log(TotalAmount);
-
-//     const totalAmount = await TotalAmount.map((item) => {
-//       const totalPrice = item.price.map(
-//         (price, index) => price * item.qty[index]
-//       );
-//       return totalPrice;
-//     });
-
-//     const sum = totalAmount.reduce((accumulator, innerArray) => {
-//       const innerSum = innerArray.reduce(
-//         (innerAccumulator, currentValue) => innerAccumulator + currentValue,
-//         0
-//       );
-//       return accumulator + innerSum;
-//     }, 0);
-
-//     return res
-//       .status(200)
-//       .json({ message: 'Total Amount Fetched Successfully', totalAmount: sum });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
+    return res.status(200).json({
+      message: 'Total Amount Fetched Successfully',
+      amountByUser: TotalAmount,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
--- router/history.js
+++ router/history.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const historyController = require('../controller/history');
 
-router.get('/:id', historyController.getHistoryById);
-
 router.get('/total/:id', historyController.totalAmountByUser);
 
-// router.get('', historyController.totalAmountForAllUser);
+router.get('/totalAmounts', historyController.totalAmountForAllUser);
+
+router.get('/:id', historyController.getHistoryById);
 
 router.post('', historyController.createHistory);
 
